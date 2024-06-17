2c2b8495ea5e65d96d4b626eaa6737420d230d3f
Nalin Angrish <nalin@nalinangrish.me>
2024-06-17 18:48:27 +0530

    Delete functionality enabled as well

--- src/components/Blog/table.jsx
+++ src/components/Blog/table.jsx
@@ -6,6 +6,7 @@ import TruncateText from "../utils/truncateText";
 import { IconPlus } from "@tabler/icons-react";
 import { getAllBlogs } from "@/app/actions/BlogData";
 import { auth } from "@/auth";
+import { DeleteBlogBtn, UpdateBlogBtn } from "./buttons";
 
 export default async function Table(props) {
   
@@ -60,67 +61,9 @@ export default async function Table(props) {
                 <td className="whitespace-wrap py-3">{rest.author}</td>
                 <td className="whitespace-wrap py-3">{rest.club}</td>
                 <td className="whitespace-wrap py-3 editButton">
-                  <form
-                    className="inline"
-                    action={`/dashboard/${props.page}/${rest.id}/edit`}
-                  >
-                    <button className="mr-1">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        className="icon inline icon-tabler icons-tabler-outline icon-tabler-edit"
-                      >
-                        <path
-                          stroke="none"
-                          d="M0 0h24v24H0z"
-                          fill="none"
-                        />
-                        <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" />
-                        <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" />
-                        <path d="M16 5l3 3" />
-                      </svg>
-                    </button>
-                  </form>
-
-                  <span className="font-bold mr-1 slash">/</span>
-                  <form
-                    className="inline"
-                    action={`/dashboard/${props.page}/${rest.id}/delete`}
-                    method="DELETE"
-                  >
-                    <button>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        className="icon inline icon-tabler icons-tabler-outline icon-tabler-trash"
-                      >
-                        <path
-                          stroke="none"
-                          d="M0 0h24v24H0z"
-                          fill="none"
-                        />
-                        <path d="M4 7l16 0" />
-                        <path d="M10 11l0 6" />
-                        <path d="M14 11l0 6" />
-                        <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-                        <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
-                      </svg>
-                    </button>
-                  </form>
+                    <UpdateBlogBtn id={rest.id} />
+                    <span className="font-bold mr-1 slash">/</span>
+                    <DeleteBlogBtn id={rest.id} />
                 </td>
               </tr>
             );
