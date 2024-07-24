29338b805f056838d456c4ad041528916f0f5feb
Nalin Angrish <nalin@nalinangrish.me>
2024-07-24 16:54:03 +0530

    Prep for Prod

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
     images: {
       domains: ['utfs.io',"example.com","images.unsplash.com"],
     },
+    basePath: process.env.NODE_ENV == "production" ? "/bost" : "/"
   };
   
   export default nextConfig;
